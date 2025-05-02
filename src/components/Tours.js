f1bb30f5368d3182aeff07a45b0d6ad096da2275
Nisarg Patel <nisargpatel1233@gmail.com>
2025-05-02 17:16:19 +0530

    made responsive

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,7 +3,7 @@ import Card from "./Card.js";
 
 function Tours({ tours ,removeTour}) {
   return (
-    <div className="container ml-[10rem] ">
+    <div className="container flex flex-wrap justify-center ">
 
       <h2 className="heading">Plan With Me</h2>
 
